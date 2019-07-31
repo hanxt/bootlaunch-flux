19605be9f882ee1120929233ef03163b7bea7fce
hanxt <hanxiaotongtong@163.com>
2019-07-31 11:22:55 +0800

    16.1.webflux快速入门

---
+++ src/main/java/com/zimug/bootlaunch/flux/BootlaunchFluxApplication.java
@@ -0,0 +1,13 @@
+package com.zimug.bootlaunch.flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlaunchFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootlaunchFluxApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zimug/bootlaunch/flux/service/HelloHandler.java
@@ -0,0 +1,16 @@
+package com.zimug.bootlaunch.flux.service;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class HelloHandler {
+    public Mono<ServerResponse> helloCity(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                .body(BodyInserters.fromObject("Hello, City!"));
+    }
+}
